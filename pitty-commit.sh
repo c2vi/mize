ebc47934dcdd25267bf80fcb7ca5cf3b727813c1
Sebastian Moser <sewi.moser@gmail.com>
2022-09-14 00:39:28 +0200

    I am sorry. #pitty-commit

---
+++ pitty-commit.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# In order to get things done I forced myself to do at least one commit a day to any of my projects on github. So if I don't get anything done on a day, I have to do a shamefull empty commit.
+
+date +"%a %b %d" >> pitty-commit.sh
+git add pitty-commit.sh
+git commit -m "I am sorry. #pitty-commit"
+
+# a list of all the dates I did nothing on
+Wed Sep 14
